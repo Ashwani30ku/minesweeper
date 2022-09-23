8a5231d92f223ac47c42584ad204a6962aaf30bf
AshwaniCoding <98018554+Ashwani30ku@users.noreply.github.com>
2022-09-23 11:36:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -109,49 +109,49 @@ document.addEventListener('DOMContentLoaded', () => {
       setTimeout(() => {
         if (currentId > 0 && !isLeftEdge) {
           const newId = squares[parseInt(currentId) -1].id
-          //const newId = parseInt(currentId) - 1   ....refactor
+          
           const newSquare = document.getElementById(newId)
           click(newSquare)
         }
         if (currentId > 9 && !isRightEdge) {
           const newId = squares[parseInt(currentId) +1 -width].id
-          //const newId = parseInt(currentId) +1 -width   ....refactor
+          
           const newSquare = document.getElementById(newId)
           click(newSquare)
         }
         if (currentId > 10) {
           const newId = squares[parseInt(currentId -width)].id
-          //const newId = parseInt(currentId) -width   ....refactor
+          
           const newSquare = document.getElementById(newId)
           click(newSquare)
         }
         if (currentId > 11 && !isLeftEdge) {
           const newId = squares[parseInt(currentId) -1 -width].id
-          //const newId = parseInt(currentId) -1 -width   ....refactor
+          
           const newSquare = document.getElementById(newId)
           click(newSquare)
         }
         if (currentId < 98 && !isRightEdge) {
           const newId = squares[parseInt(currentId) +1].id
-          //const newId = parseInt(currentId) +1   ....refactor
+          
           const newSquare = document.getElementById(newId)
           click(newSquare)
         }
         if (currentId < 90 && !isLeftEdge) {
           const newId = squares[parseInt(currentId) -1 +width].id
-          //const newId = parseInt(currentId) -1 +width   ....refactor
+          
           const newSquare = document.getElementById(newId)
           click(newSquare)
         }
         if (currentId < 88 && !isRightEdge) {
           const newId = squares[parseInt(currentId) +1 +width].id
-          //const newId = parseInt(currentId) +1 +width   ....refactor
+         
           const newSquare = document.getElementById(newId)
           click(newSquare)
         }
         if (currentId < 89) {
           const newId = squares[parseInt(currentId) +width].id
-          //const newId = parseInt(currentId) +width   ....refactor
+          
           const newSquare = document.getElementById(newId)
           click(newSquare)
         }
